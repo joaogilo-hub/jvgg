3bafad6e5749ce8d2d3280e18adc8aa3874a3701
joaogilo-hub <joao.gilo@edu.senai.br>
2026-08-18 11:13:42 -0300

    Finalizado

---
+++ Aula-01/PrimeiroMVC/Controllers/CasdatroController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC2026;
+
+public class CadastrController : Controller
+{
+    public IActionResult Aluno ()
+    {
+        return View();
+
+    }
+}
